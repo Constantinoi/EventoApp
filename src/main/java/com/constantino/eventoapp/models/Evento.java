f32b8588f509f1b937818c53471c6da74e74ebe8
Constantinoi <pescaalma@gmail.com>
2021-04-08 20:19:27 -0300

    Relacionando entidades utilizando @OneToMany e @ManyToOne

--- src/main/java/com/constantino/eventoapp/models/Evento.java
+++ src/main/java/com/constantino/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.constantino.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento  implements Serializable{
@@ -20,6 +22,9 @@ public class Evento  implements Serializable{
 	private String local;
 	private String data;
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
@@ -51,6 +56,11 @@ public class Evento  implements Serializable{
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
-	
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+	public void setConvidados(List<Convidado> convidados) {
+		this.convidados = convidados;
+	}
 	
 }
